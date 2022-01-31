f92f0a7308b26f0c28baa4e14d808c02edef4e48
Cheyenne Ashou <cashou985@gmail.com>
2022-01-31 10:10:43 -0600

    HW 2

--- rubik/check.py
+++ rubik/check.py
@@ -1,10 +1,54 @@
 import rubik.cube as rubik
+from curses.ascii import isalnum
 
 def _check(parms):
     result={}
-    encodedCube = parms.get('cube',None)       
+    encodedCube = parms.get('cube',None)
+
     if(encodedCube == None):
-        result['status'] = 'error: xxx'
+        result['status'] = 'error: No value for "cube"\n'
+        return result
     else:
         result['status'] = 'ok'
+    
+    if(len(encodedCube) < 54):
+        result['status'] = 'error: Length for "cube" is too small\nThere must be 54 elements.'
+        return result
+    elif len(encodedCube) > 54:
+        result['status'] = 'error: Length for "cube" is too big\nThere must be 54 elements.'
+        return result
+        
+    if encodedCube.isalnum() == False:
+        result['status'] = 'error: Non-alphanumber character used in the value for "cube"'
+        return result
+    
+    colors = {}
+    midColors = {}
+    index = 0;
+    # Count occurrences of each color
+    for elementColor in encodedCube:
+        if(index == 4 or (index > 4 and (index - 4) % 9 == 0)):
+            if(elementColor in midColors):
+                midColors[elementColor] += 1
+            else: midColors[elementColor] = 0
+        if(elementColor in colors):
+            colors[elementColor] += 1
+        else:
+            colors[elementColor] = 1
+        index += 1
+    if len(colors) != 6:
+        result['status'] = 'error: There must be 6 colors'
+        return result
+                
+    # Check if any elementColor has more or less then 9
+    for elementColor in colors:
+        if colors[elementColor] != 9:
+            result['status'] = 'error: Each color must have 9 occurrences'
+            return result
+        
+    # Check if there are middle colors that are the same
+    if len(midColors) != 6:
+        result['status'] = 'error: 2 or more middle pieces have the same color'
+        return result
+    
     return result
--- rubik/test/checkTest.py
+++ rubik/test/checkTest.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 import rubik.check as check 
 
 class CheckTest(TestCase):
-        
+    #Happy
     def test_check_010_ShouldReturnOkOnSolvedCube(self):
         parm = {'op':'check',
                 'cube':'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'}
@@ -10,4 +10,77 @@ class CheckTest(TestCase):
         self.assertIn('status', result)
         status = result.get('status', None)
         self.assertEqual(status, 'ok')
+
+    def test_check_011_ShouldReturnOkOnSolvedCube(self):
+        parm = {'op':'check',
+                'cube':'bbbbrbbbbrrrrbrrrrggggyggggoooowooooyyyygyyyywwwwowwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'ok')
+    
+    def test_check_012_ShouldReturnOkOnSolvedCube(self):
+        parm = {'op':'check',
+                'cube':'bbbbbbbbbBBBBBBBBBgggggggggoooooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'ok')
+    
+    #Sad test paths
+    def test_check_013_ShouldReturnErrorNoCube(self):
+        parm = {'op':'check',
+                'cube': None}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: No value for "cube"\n')
+    
+    def test_check_014_ShouldReturnErrorTooLong(self):
+        parm = {'op':'check',
+                'cube':'1113222rrr111bbb222ooo111222rrrbbbooorrrbbbooowwwwwwwwwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: Length for "cube" is too big\nThere must be 54 elements.')
+    
+    def test_check_015_ShouldReturnErrorTooSmall(self):
+        parm = {'op':'check',
+                'cube':'113222rrr111bbb222ooo111222rrrbbbooo'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: Length for "cube" is too small\nThere must be 54 elements.')
+    
+    def test_check_016_ShouldReturnErrorMiddlePieceConflict(self):
+        parm = {'op':'check',
+                'cube':'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyywwwwwywwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: 2 or more middle pieces have the same color')
+    
+    def test_check_017_ShouldReturnErrorNotSixColors(self):
+        parm = {'op':'check',
+                'cube':'bbbbbbbbxrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: There must be 6 colors')
+    
+    def test_check_018_ShouldReturnErrorNotNineOccurences(self):
+        parm = {'op':'check',
+                'cube':'bbbbbbbbrrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: Each color must have 9 occurrences')
+        
+    def test_check_019_ShouldReturnErrorNonAlphaNumeric(self):
+        parm = {'op':'check',
+                'cube':'000000000rr@rrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: Non-alphanumber character used in the value for "cube"')
 
