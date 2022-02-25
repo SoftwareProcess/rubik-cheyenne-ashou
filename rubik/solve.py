0d6a06b172340feba8dc1d8ed8fd1501d2bd2ef8
Cheyenne Ashou <cashou985@gmail.com>
2022-02-24 20:06:20 -0600

    Green Light |

--- rubik/solve.py
+++ rubik/solve.py
@@ -2,9 +2,8 @@ import rubik.cube as rubik
 
 def _solve(parms):
     result = {}
-    encodedCube = parms.get('cube',None)       #get "cube" parameter if present
-    result['solution'] = 'FfRrBbLlUuDd'        #example rotations
-    result['status'] = 'ok'                     
+    result['cube'] = 'bwbybgrygyogyrrobwogrbgooggbwyworwogwwybygrroyowbwyrrb'            
+    result['status'] = 'ok'
     return result
 
 # dev strategy
