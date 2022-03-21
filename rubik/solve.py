f68e42d2709c61e8c212000cd0a89888c5312c97
Cheyenne Ashou <cashou985@gmail.com>
2022-03-20 19:17:34 -0500

    Green Light |

--- rubik/solve.py
+++ rubik/solve.py
@@ -100,43 +100,8 @@ def _bottomFlower(myCube):
             flowerPiecesOnFace = True
             face = (face + 1) % 4
     
-    matching = False
-    for face in range(0,4):
-        while(matching == False):
-            if(face == 0):
-                if(myCube._content[face][0][1] == myCube._content[face][1][1] and myCube._content[4][2][1] == bottomFaceColor):
-                    myCube._content = _movecontroller(myCube, 'FF')
-                    moves += _movetranslator(face, 'FF')
-                    matching = True
-                else:
-                    myCube._content = _movecontroller(myCube, 'U')
-                    moves += _movetranslator(face, 'U')
-            elif(face == 1):
-                if(myCube._content[face][0][1] == myCube._content[face][1][1] and myCube._content[4][1][2] == bottomFaceColor):
-                    myCube._content = _movecontroller(myCube, 'RR')
-                    moves += _movetranslator(face, 'RR')
-                    matching = True
-                else:
-                    myCube._content = _movecontroller(myCube, 'U')
-                    moves += _movetranslator(face, 'U')
-            elif(face == 2):
-                if(myCube._content[face][0][1] == myCube._content[face][1][1] and myCube._content[4][0][1] == bottomFaceColor):
-                    myCube._content = _movecontroller(myCube, 'BB')
-                    moves += _movetranslator(face, 'BB')
-                    matching = True
-                else:
-                    myCube._content = _movecontroller(myCube, 'U')
-                    moves += _movetranslator(face, 'U')
-            elif(face == 3):
-                if(myCube._content[face][0][1] == myCube._content[face][1][1] and myCube._content[4][1][0] == bottomFaceColor):
-                    myCube._content = _movecontroller(myCube, 'LL')
-                    moves += _movetranslator(face, 'LL')
-                    matching = True
-                else:
-                    myCube._content = _movecontroller(myCube, 'U')
-                    moves += _movetranslator(face, 'U')
-        matching = False
-   
+    moves += _formBottomCross(myCube)
+    
     return moves
 
 def _formBottomCross(myCube):
