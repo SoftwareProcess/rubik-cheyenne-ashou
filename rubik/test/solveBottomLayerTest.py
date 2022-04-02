c130219649f332919074fda6a1a67b131dfe8561
Cheyenne Ashou <cashou985@gmail.com>
2022-04-02 15:46:28 -0500

    Red Light |

--- rubik/test/solveBottomLayerTest.py
+++ rubik/test/solveBottomLayerTest.py
@@ -121,6 +121,25 @@ class BottomLayerTest(unittest.TestCase):
         actualCornerCoord = bottomLayer._getCornerPiece(content)
         
         self.assertEqual(expectedCornerCoord, actualCornerCoord)
+        
+    def test_023_getCornerPiece_shouldReturnBottomRightCornerPieceCoordinateBecauseWrongOrientation(self):
+        inputDict = {}
+        inputDict['op'] = 'solve'
+        inputDict['cube'] = 'rowrrbrrwggbygobggooogoyoooyybrbbybggbwgyryyrywrwwwbww'
+        
+        myCube = cube.Cube()
+        myCube._load(inputDict['cube'])
+        content = myCube._getContent()
+        
+        expectedCheck = {'status': 'ok'}
+        actualCheck = check._check(inputDict)
+        self.assertEqual(expectedCheck, actualCheck)
+        
+        expectedCornerCoord = (2,2) 
+        actualCornerCoord = bottomLayer._getCornerPiece(content)
+        
+        self.assertEqual(expectedCornerCoord, actualCornerCoord)
+    
     
     
     
