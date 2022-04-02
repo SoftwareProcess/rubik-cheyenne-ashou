faf0ca3458b739fe18da6eb34fccfdda9ba57cb5
Cheyenne Ashou <cashou985@gmail.com>
2022-04-02 18:15:55 -0500

    Yellow Light |

--- rubik/test/solveTest.py
+++ rubik/test/solveTest.py
@@ -131,21 +131,26 @@ class SolveTest(unittest.TestCase):
         self.assertEqual(expectedContent, actualContent)
   
         
-    @unittest.skip("skip while changing the movecontroller method")    
+ 
     def test_solve_022_ShouldRotateValidNominalCubeL(self):
         inputDict = {}
         inputDict['cube'] = 'rrrrrrrrrgggggggggooooooooobbbbbbbbbyyyyyyyyywwwwwwwww'
         inputDict['rotate'] = 'L'
         inputDict['op'] = 'solve'
         
-        expectedResult = {}
-        expectedResult['cube'] = 'yrryrryrrgggggggggoowoowoowbbbbbbbbboyyoyyoyyrwwrwwrww'
-        expectedResult['status'] = 'ok'
+        myCube = cube.Cube()
+        myCube._load(inputDict['cube'])
+        content = myCube._getContent()
         
-        actualResult = solve._solve(inputDict)
+        moves = inputDict['rotate']
+        rotatedCube = 'yrryrryrrgggggggggoowoowoowbbbbbbbbboyyoyyoyyrwwrwwrww'
+        myCube2 = cube.Cube()
+        myCube2._load(rotatedCube)
+        expectedContent = myCube2._getContent()
+        actualContent = solve._movecontroller(content, moves)
         
-        self.assertEqual(expectedResult.get('cube'), actualResult.get('cube'))
-        self.assertEqual(expectedResult.get('status'), actualResult.get('status'))
+        self.assertEqual(expectedContent, actualContent)
+        
     
     @unittest.skip("skip while changing the movecontroller method")    
     def test_solve_023_ShouldRotateValidNominalCubeU(self):
