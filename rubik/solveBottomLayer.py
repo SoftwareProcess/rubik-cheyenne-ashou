cb79359f8dcd8dd21f6a0c5b64f75f6976cd5099
Cheyenne Ashou <cashou985@gmail.com>
2022-04-01 21:23:00 -0500

    Green Light |

--- rubik/solveBottomLayer.py
+++ rubik/solveBottomLayer.py
@@ -28,6 +28,49 @@ def _checkSolved(content):
             if piece != bottomLayerColor:
                 solved = False
                 break
-    
+
     return solved
+
+def _getCornerPiece(content):
+    
+    frontCorners = {
+            'topLeft': content[0][0][0]
+            , 'topRight': content[0][0][2]
+            , 'bottomLeft': content[0][2][0]
+            , 'bottomRight': content[0][2][2]
+            }
+    
+    adjacentLeftCorners = {'upper': content[4][2][0], 'left': content[3][0][2],
+                           'bottomLeft': content[3][2][2], 'lower': content[5][0][0]}
+    adjacentRightCorners = {'upper': content[4][2][2], 'right': content[1][0][0],
+                            'bottomRight': content[1][2][0], 'lower': content[5][0][2]}
+    
+    bottomFaceColor = content[5][1][1]
+    frontFaceColor = content[0][1][1]
+    leftFaceColor = content[3][1][1]
+    rightFaceColor = content[1][1][1]
+    
+    if(frontCorners['topLeft'] == frontFaceColor 
+       and adjacentLeftCorners['upper'] == leftFaceColor
+       and adjacentLeftCorners['left'] == bottomFaceColor):
+        return (0,0)
+    elif(frontCorners['topRight'] == frontFaceColor
+         and adjacentRightCorners['upper'] == rightFaceColor
+         and adjacentRightCorners['right'] == bottomFaceColor):
+        return (0,2)
+    elif(frontCorners['bottomLeft'] == bottomFaceColor): #do luL
+        return (2,0)
+    elif(frontCorners['bottomRight'] == bottomFaceColor): #do RUr
+        return (2,2)
+    elif(adjacentLeftCorners['lower'] == bottomFaceColor
+         and (adjacentLeftCorners['bottomLeft'] != leftFaceColor
+              or frontCorners['bottomLeft'] != frontFaceColor)):
+        return (2,0)
+    elif(adjacentRightCorners['lower'] == bottomFaceColor
+         and (adjacentRightCorners['bottomRight'] != rightFaceColor
+              or frontCorners['bottomRight'] != frontFaceColor)):
+        return (2,2)
+    
+    
+    
         
