14fad1afa160590b930c0a143527394bd08e6f08
Cheyenne Ashou <cashou985@gmail.com>
2022-04-04 17:32:49 -0500

    Green Light |

---
+++ rubik/test/solveBottomCrossTest.py
@@ -0,0 +1,243 @@
+'''
+Created on Apr 4, 2022
+
+@author: cheyennea.
+'''
+import unittest
+import rubik.cube as cube
+import rubik.solve as solve
+import rubik.solveBottomLayer as bottomLayer
+
+class SolveBottomLayerTest(unittest.TestCase):
+    def test_solve_130_ShouldReturnEmptySolutionForUnmixedValidCube(self):
+        inputDict = {}
+        inputDict['op'] = 'solve'
+        inputDict['cube'] = 'rrrrrrrrrgggggggggooooooooobbbbbbbbbyyyyyyyyywwwwwwwww'
+        
+        expectedResult = {}
+        expectedResult['solution'] = ''
+        expectedResult['status'] = 'ok'
+        
+        myCube = cube.Cube()
+        myCube._load(inputDict['cube'])
+        
+        actualResult = solve._solve(inputDict)
+        #actualResult = solve._bottomcross(myCube)
+        
+        self.assertEqual(expectedResult.get('solution'), actualResult.get('solution'))
+        self.assertEqual(expectedResult.get('status'), actualResult.get('status'))
+        self.assertEqual(expectedResult, actualResult)
+    
+    
+    def test_solve_131_ShouldSolveBottomCrossForMixedValidCubeMissingRotation(self):
+        inputDict = {}
+        inputDict['op'] = 'solve'
+        #inputDict['cube'] = 'rgrwgoybogbrwowyboyyorbybgwgygorbbggwrbgyowryorbwwyrow'
+        inputDict['cube'] = 'brborgyboywgrggwowoogyowbywrwybbyoorwbyrygrbogygwwgbrr'
+        bottomFaceColor = inputDict['cube'][49]
+        
+        moves = solve._solve(inputDict).get('solution')
+        print(moves)
+        inputDict['rotate'] = moves
+        print(moves)
+        actualResult = solve._solve(inputDict)
+        
+        expectedStatus = 'ok'
+        
+        self.assertEqual(expectedStatus, actualResult['status'])
+        
+        myCube = cube.Cube()
+        myCube._load(actualResult.get('cube'))
+        content = myCube._getContent()
+        print(content)
+        expectedSolvedResult = True
+        actualSolvedResult = bottomLayer._checkSolved(content)
+        self.assertEqual(expectedSolvedResult, actualSolvedResult)
+        
+    
+    
+    def test_solve_132_ShouldSolveBottomCrossForMixedValidCubeEmptyRotation(self):
+        inputDict = {}
+        inputDict['op'] = 'solve'
+        inputDict['cube'] = 'oowrboyyrowobygwrbywgygroywygbgwyrrbrbgoogwwgrobwrbgby'
+        inputDict['rotate'] = ''
+        
+        moves = solve._solve(inputDict).get('solution')
+        print(moves)
+        inputDict['rotate'] = moves
+        print(moves)
+        actualResult = solve._solve(inputDict)
+        
+        expectedStatus = 'ok'
+        
+        self.assertEqual(expectedStatus, actualResult['status'])
+        
+        myCube = cube.Cube()
+        myCube._load(actualResult.get('cube'))
+        content = myCube._getContent()
+        print(content)
+        expectedSolvedResult = True
+        actualSolvedResult = bottomLayer._checkSolved(content)
+        self.assertEqual(expectedSolvedResult, actualSolvedResult)
+        
+    
+    
+    def test_solve_133_ShouldSolveBottomCrossForPartiallySolvedCrossValidCube(self):
+        inputDict = {}
+        inputDict['op'] = 'solve'
+        inputDict['cube'] = 'ggrrgwbooyroboowobwrrybgroywyyrrbgbygwbgyyowboggywwrbw'
+        inputDict['rotate'] = ''
+        
+        bottomFaceColor = inputDict['cube'][49]
+        
+        moves = solve._solve(inputDict).get('solution')
+        
+        inputDict['rotate'] = moves
+        
+        actualResult = solve._solve(inputDict)
+        expectedStatus = 'ok'
+        
+        # self.assertEqual(expectedStatus, actualResult['status'])
+        # self.assertEqual(actualResult['cube'][46], bottomFaceColor)
+        # self.assertEqual(actualResult['cube'][7], actualResult['cube'][4])
+        # self.assertEqual(actualResult['cube'][48], bottomFaceColor)
+        # self.assertEqual(actualResult['cube'][34], actualResult['cube'][31])
+        # self.assertEqual(actualResult['cube'][50], bottomFaceColor)
+        # self.assertEqual(actualResult['cube'][16], actualResult['cube'][13])
+        # self.assertEqual(actualResult['cube'][52], bottomFaceColor)
+        # self.assertEqual(actualResult['cube'][25], actualResult['cube'][22])
+        #
+
+   
+    def test_rotateMiddle_140_ShouldRotateMiddleLayer(self):
+        inputDict = {}
+        inputDict['cube'] = 'rrrrrrrrrgggggggggooooooooobbbbbbbbbyyyyyyyyywwwwwwwww'
+        inputDict['op'] = 'solve'
+        myCube = cube.Cube()
+        myCube._load(inputDict['cube'])
+        content = myCube._getContent()
+        
+   
+        expectedResult = 'rrrgggrrrgggooogggooobbbooobbbrrrbbbyyyyyyyyywwwwwwwww'
+        
+        myCube._content = solve._rotateMiddle(content)
+        actualResult = myCube._get()
+        
+        self.assertEqual(expectedResult, actualResult)
+    
+    
+    def test_rotateCubeClockwise_150_ShouldRotateEntireCubeClockWise(self):
+        inputDict = {}
+        inputDict['cube'] = 'rrrrrrrrrgggggggggooooooooobbbbbbbbbyyyyyyyyywwwwwwwww'
+        inputDict['op'] = 'solve'
+        myCube = cube.Cube()
+        myCube._load(inputDict['cube'])
+        content = myCube._getContent()
+        
+        expectedContent = 'gggggggggooooooooobbbbbbbbbrrrrrrrrryyyyyyyyywwwwwwwww'
+        myCube2 = cube.Cube()
+        myCube2._load(expectedContent)
+        expectedContent = myCube2._getContent()
+        
+        actualContent = solve._rotateCubeClockwise(content)
+ 
+        self.assertEqual(expectedContent, actualContent)
+        
+    def test_movetranslator_160_ShouldTranslateMovesBasedOnCurrentFace(self):
+        inputMoves = 'FfBbLlRrUuDd'
+        face = 1
+        expectedMoves = 'RrLlFfBbUuDd'
+        actualMoves = solve._movetranslator(face, inputMoves)
+        self.assertEqual(expectedMoves, actualMoves)
+        
+        face = 2
+        expectedMoves = 'BbFfRrLlUuDd'
+        actualMoves = solve._movetranslator(face, inputMoves)
+        self.assertEqual(expectedMoves, actualMoves)
+        
+        face = 3
+        expectedMoves = 'LlRrBbFfUuDd'
+        actualMoves = solve._movetranslator(face, inputMoves)
+        self.assertEqual(expectedMoves, actualMoves)
+        
+        face = 4
+        expectedMoves = 'UuDdLlRrBbFf'
+        actualMoves = solve._movetranslator(face, inputMoves)
+        self.assertEqual(expectedMoves, actualMoves)
+        
+        face = 5
+        expectedMoves = 'DdUuLlRrFfBb'
+        actualMoves = solve._movetranslator(face, inputMoves)
+        self.assertEqual(expectedMoves, actualMoves)
+
+    
+    def test_170_formBottomCross_ShouldFormBottomCrossGivenCubeHasTopFlower(self):
+        inputDict = {}
+        inputDict['op'] = 'solve'
+        inputDict['cube'] = 'wrwrbboyygbborobbrrggygrwygooogoyrggywywywbwrwgoowrybb'
+        
+        myCube = cube.Cube()
+        myCube._load(inputDict['cube'])
+        content = myCube._getContent()
+        bottomFaceColor = inputDict['cube'][49]
+        
+        moves = solve._formBottomCross(content)
+        inputDict['rotate'] = moves
+        actualResult = solve._solve(inputDict)
+        
+    
+        solve._solve(inputDict)
+        
+        self.assertEqual(actualResult['cube'][46], bottomFaceColor)
+        self.assertEqual(actualResult['cube'][7], actualResult['cube'][4])
+        self.assertEqual(actualResult['cube'][48], bottomFaceColor)
+        self.assertEqual(actualResult['cube'][34], actualResult['cube'][31])
+        self.assertEqual(actualResult['cube'][50], bottomFaceColor)
+        self.assertEqual(actualResult['cube'][16], actualResult['cube'][13])
+        self.assertEqual(actualResult['cube'][52], bottomFaceColor)
+        self.assertEqual(actualResult['cube'][25], actualResult['cube'][22])
+    
+    
+    def test_formBottomCross_171_ShouldDoNothingBecuaseCrossAlreadySolved(self):
+        myCube = cube.Cube()
+        myCube._load('rrrrrrrrrgggggggggooooooooobbbbbbbbbyyyyyyyyywwwwwwwww')
+        content = myCube._getContent()
+        expectedMoves = ''
+        moves = solve._formBottomCross(content)
+        actualMoves = moves
+        self.assertEqual(expectedMoves, actualMoves)
+    
+    
+    def test_solvedFlower_180_ShouldReturnTrueIfTopLayerHasFlower(self):
+        inputDict = {}
+        inputDict['op'] = 'solve'
+        inputDict['cube'] = 'gboyoooogwygybrwbrorwgrobyyogobgrbggbwywygywbwwrowroby'
+        expectedResult = True
+        myCube = cube.Cube()
+        myCube._load(inputDict['cube'])
+        content = myCube._getContent()
+        actualResult = solve._solvedFlower(content)
+        self.assertEqual(expectedResult, actualResult)
+        
+        inputDict['cube'] = 'rrrrrrrrrgggggggggooooooooobbbbbbbbbyyyyyyyyywwwwwwwww'
+        expectedResult = True
+        myCube2 = cube.Cube()
+        myCube2._load(inputDict['cube'])
+        content2 = myCube2._getContent()
+        actualResult = solve._solvedFlower(content2)
+        self.assertEqual(expectedResult, actualResult)
+    
+    
+    def test_solvedFlower_181_ShouldReturnFalseBecauseTopFlowerNotSolved(self):
+        inputDict = {}
+        inputDict['op'] = 'solve'
+        inputDict['cube'] = 'wyyrrbgbyggrrgybowbowooboryorrwbgroygwwwywggoryobwgbyb'
+        expectedResult = False
+        myCube = cube.Cube()
+        myCube._load(inputDict['cube'])
+        content = myCube._getContent()
+        
+        actualResult = solve._solvedFlower(content)
+        self.assertEqual(expectedResult, actualResult)
+        
+
