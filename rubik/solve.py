0513c79d647ec6c6edfa83747171f78fa2a02348
Cheyenne Ashou <cashou985@gmail.com>
2022-04-04 15:46:43 -0500

    Red Light |

--- rubik/solve.py
+++ rubik/solve.py
@@ -28,8 +28,9 @@ def _solve(parms):
     
     if(moves == '' or moves == None):
         result['solution'] = _topFlower(content)
-        bottomCornersContent = content[:]    
-        result['solution'] += bottomLayer._movesToPlaceCornerPieces(bottomCornersContent)
+        bottomCornersContent = content[:]
+        print(bottomCornersContent)    
+        #result['solution'] += bottomLayer._movesToPlaceCornerPieces(bottomCornersContent)
     else:
         myCube._content = _movecontroller(content, moves)
         result['cube'] = myCube._get()
