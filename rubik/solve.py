f8aa308a488777ab09d1734be693030c19a522e4
Cheyenne Ashou <cashou985@gmail.com>
2022-04-03 20:42:51 -0500

    Red Light |

--- rubik/solve.py
+++ rubik/solve.py
@@ -211,7 +211,6 @@ def _rotateMiddle(content):
     return content
 
 def _rotateCubeClockwise(content):
-    content = content[:]
     content = _movecontroller(content, 'UMd')
     return content 
 
@@ -397,7 +396,6 @@ def _translateFace5(move):
     
 #Rotate cube
 def _movecontroller(content, moves):
-    content = content[:]
     for move in moves:
         if move == 'F':
             face = 0
@@ -465,7 +463,6 @@ def _movecontroller(content, moves):
         
 
 def _clockwise(face):
-    face = face[:]
     rowlength = len(face)
     for row in range(0, int(rowlength / 2)):
         for col in range(row, rowlength-1-row):
@@ -478,7 +475,6 @@ def _clockwise(face):
 
 
 def _counterclockwise(face):
-    face = face[:]
     rowlength = len(face[0])
     for row in range(0, int(rowlength / 2)):
         for col in range(row, rowlength-1-row):
@@ -490,7 +486,6 @@ def _counterclockwise(face):
     return face
 
 def _switchedge(cube, action):
-    cube = cube[:]
     if (action == 'F' or action == 'f'):
         temp = cube[4][2]
         cube[4][2] = [cube[3][2][2], cube[3][1][2], cube[3][0][2]]
