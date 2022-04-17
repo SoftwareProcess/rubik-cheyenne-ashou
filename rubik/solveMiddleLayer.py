0cecb38ab286962c8d779ad858538646306e3397
Cheyenne Ashou <cashou985@gmail.com>
2022-04-17 16:43:17 -0500

    Green Light |

--- rubik/solveMiddleLayer.py
+++ rubik/solveMiddleLayer.py
@@ -3,5 +3,14 @@ Created on Apr 17, 2022
 
 @author: cheyennea.
 '''
+import rubik.check as check
 
-
+def _checkSolved(content):
+    solved = True
+    bottomSolved = check.checkBottomLayerSolved(content)
+    middleSolved = check.checkMiddleLayerSolved(content)
+    
+    if(bottomSolved == False or middleSolved == False):
+        solved = False
+    
+    return solved
