8809d264e6555b03b9808b1f31f13bb201df83cc
Cheyenne Ashou <cashou985@gmail.com>
2022-04-19 18:16:01 -0500

    Green Light |

--- rubik/test/solveMiddleLayerTest.py
+++ rubik/test/solveMiddleLayerTest.py
@@ -585,7 +585,7 @@ class SolveMiddleLayerTest(unittest.TestCase):
         expectedResult = ''
         edgesPlaced = _checkGoToNextFace(content)
         rightEdgePlaced = edgesPlaced['rightEdge']
-        self.assertEqual(rightEdgePlaced, True)
+        self.assertEqual(rightEdgePlaced, False)
         actualResult = middleLayer._insertRightEdge(content, rightEdgePlaced)
         self.assertEqual(expectedResult, actualResult)
     
